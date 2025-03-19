d23c69993a85946535f3b89c0a0d1095acaf094a
Justin <125344973+Justin82000@users.noreply.github.com>
2025-03-19 11:30:19 +0100

    TP2-7 fonctionne mais qui peux me stopper en ft je suis le jesus francais ou quoi

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,21 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infoMarche = controlAfficherMarche.donnerInfoMarche();
+		if (infoMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard");
+		}
+		else {
+			System.out.println(nomAcheteur + " ,vous trouverez au marché :\n");
+			for (int i = 0; i < infoMarche.length; i++) {
+				String Vendeur = infoMarche[i];
+				i++;
+				String Quantite = infoMarche[i];
+				i++;
+				String Produit = infoMarche[i];
+				i++;
+				System.out.println("-" + Vendeur + " qui vend" + Quantite + " " + Produit);
+			}
+		}
 	}
 }
